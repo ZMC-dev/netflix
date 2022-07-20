c2830b5cdc0ffd113d46c47ae02f2c9e74312510
ZMC-dev <zmc.dev2019@gmail.com>
2022-07-20 17:17:48 +0200

    code works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import database from'./data/database.json';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  //const movies = [];
+  return(
+    <div className='App'>
+       <h1>NETFLIX</h1>
+       {database.map((movies) => {
+        return (
+          <div>
+            <p className='movieCategory'>{movies.category}</p>
+            <div> 
+            {movies.images.map((urlImages) => {
+              return <img src={urlImages} alt=""></img>
+            }
+            )}
+            </div>
+          </div>
+        )
+      })
+  }
     </div>
   );
 }
-
 export default App;
+
+
